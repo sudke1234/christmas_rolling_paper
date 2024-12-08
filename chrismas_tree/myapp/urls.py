3df460053228677261ff5818bcc35251973f4b64
sudke1234@kakao.com <sudke1234@kakao.com>
2024-12-08 21:11:25 +0900

    decoration_file

--- chrismas_tree/myapp/urls.py
+++ chrismas_tree/myapp/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-    path('', views.index, name='index'),  ]
+    path('', views.index, name='index'),  # 기존 index 페이지
+    path('decoration_selected/', views.decoration_selected_view, name='decoration_selected'),  # 데이터 선택 페이지
+   
+]
 
--- chrismas_tree/myapp/views.py
+++ chrismas_tree/myapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
 # Create your views here.
 
 def index(request):
     return render(request, 'board/index.html')
+
+def decoration_selected_view(request):
+    return render(request, 'board/decoration_selected.html')
