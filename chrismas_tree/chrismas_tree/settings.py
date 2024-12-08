d4158da6501551f65aba9dc4f7a6b6013eb3a2d3
sudke1234@kakao.com <sudke1234@kakao.com>
2024-12-08 20:18:23 +0900

    decoration db

--- chrismas_tree/chrismas_tree/settings.py
+++ chrismas_tree/chrismas_tree/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp',
+    'myapp', 
+    'rest_framework', #drf router
+    'multiselectfield', #다중선택
 ]
 
 MIDDLEWARE = [
--- chrismas_tree/myapp/models.py
+++ chrismas_tree/myapp/models.py
@@ -1,6 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+#크리스마스 장식 db
+Decorations = (
+    (0, 'decoration_1'),
+    (1, 'decoration_2'),
+    (2, 'decoration_3'),
+)
+
+class Decoration_Choose(models.Model):
+    Decoration = models.CharField(  choices = Decorations , max_length = 1)
+
 class Post(models.Model):
     title = models.CharField(max_length = 100)
     content = models.TextField(max_length = 600)
