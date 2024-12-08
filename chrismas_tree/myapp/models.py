7cea2a5a6614e6210519530597073533612890a1
sudke1234@kakao.com <sudke1234@kakao.com>
2024-12-09 02:27:39 +0900

    complete

--- chrismas_tree/myapp/models.py
+++ chrismas_tree/myapp/models.py
@@ -2,16 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 #크리스마스 장식 db
-Decorations = (
-    (0, 'decoration_1'),
-    (1, 'decoration_2'),
-    (2, 'decoration_3'),
-)
+
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100) 
     content = models.TextField(max_length=600)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    # created = models.DateTimeField(auto_now_add=True) #생성된 날짜
+    # updated = models.DateTimeField(auto_now=True) #수정
     decoration = models.IntegerField(default=0)  # 'decoration' 필드 추가
 
     def __str__(self):
--- chrismas_tree/myapp/views.py
+++ chrismas_tree/myapp/views.py
@@ -2,25 +2,31 @@ from django.http import JsonResponse
 from .models import Post
 from django.shortcuts import render, redirect
 
-def decoration_selected_view(request):
-    if request.method == 'POST':
+# def ecoration_selectded_view(request):
+#     if request.method == 'POST':
+      
+#         title = request.POST.get('title')
+#         content = request.POST.get('content')
+#         #post 모델 저장
+#         post = Post.objects.create(title=title, content=content)
 
-        choosion = request.Post.get('decoration')
-        title = request.POST.get('title')
-        content = request.POST.get('content')
-        #post 모델 저장
-        title.save()
-        content.save()
-        choosion.save()
+#         return redirect('index')
     
-        # 출력 확인
-        print("제목:", title, "내용:", content, "장식:", decoration)
-    
-        decoration = request.POST.get('decoration', '0')
-
-    return render(request, 'board/decoration_selected.html')  # GET 요청일 경우 폼을 반환
-
+#     return render(request, 'board/decoration_selected.html')  # GET 요청일 경우 폼을 반환
 
 
 def index(request):
     return render(request, 'board/index.html')
+
+def decoration_selected_view(request):
+    return render(request, 'board/decoration_selected.html')
+
+def decoration_selectded_view(request):
+    if request.method == 'POST':
+        new_article = Post.objects.create(
+            title=request.POST['title'],
+            content=request.POST['content'],
+            decoration=int(request.POST['decoration'])  # 장식 값 추가
+        )
+    return redirect('/index/')
+    return render(request, 'board/decoration_selected.html')
